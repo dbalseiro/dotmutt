40ebf8d2995264c85099576532cf9fe0a9d300d2
Diego Balseiro <diego.balseiro@syntagma.com.ar>
2016-11-18 19:10:23 -0500

    cagada mia

--- deletegarbage.lua
+++ deletegarbage.lua
@@ -31,6 +31,7 @@ garbage =   account['[Gmail]/Todos']:contain_from('@recode.net') +
             account['[Gmail]/Todos']:contain_from('@mail.xmind.net') +
             account['[Gmail]/Todos']:contain_from('@connect.vmware.com') +
             account['[Gmail]/Todos']:contain_from('@newrelic.com') +
+            account['[Gmail]/Todos']:contain_from('mantisqa@buenosaires.gob.ar') +
             account['[Gmail]/Todos']:contain_from('@getpostman.com') +
             account['[Gmail]/Todos']:contain_from('infombf@mercedes-benz.com.ar') +
             account['[Gmail]/Todos']:contain_from('@lucidchart.com') +
@@ -42,6 +43,14 @@ garbage =   account['[Gmail]/Todos']:contain_from('@recode.net') +
             account['[Gmail]/Todos']:contain_from('openshift-email@redhat.com') +
             account['[Gmail]/Todos']:contain_from('@capacitarteuba.org') +
             account['[Gmail]/Todos']:contain_from('@codenvy.com') +
-            account['[Gmail]/Todos']:contain_to('x@mail.asana.com')
-            account['[Gmail]/Todos']:match_subject('Inscripci.n en L.nea - ')
-account['[Gmail]/Todos']:delete_messages(garbage)
+            account['[Gmail]/Todos']:contain_to('x@mail.asana.com') +
+            account['[Gmail]/Todos']:contain_to('sonia.abregu@syntagma.com.ar') +
+            account['[Gmail]/Todos']:contain_to('tomas.aguirre@syntagma.com.ar') +
+            account['[Gmail]/Todos']:contain_to('sonyabrg@gmail.com') +
+            account['[Gmail]/Todos']:contain_to('emilianoagustingallo@gmail.com')
+
+account['[Gmail]/Todos']:move_messages(account['[Gmail]/Papelera'], garbage)
+
+garbage2 = account['[Gmail]/Enviados']:contain_to('diego.balseiro@syntagma.com.ar')
+
+account['[Gmail]/Todos']:move_messages(account['[Gmail]/Papelera'], garbage2)
